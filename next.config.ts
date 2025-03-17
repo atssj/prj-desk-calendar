481fe3a9a81e27ba425949c175fc8a47abc38f59
Subhajit Saha <103457002+atssj@users.noreply.github.com>
2025-03-17 11:31:15 +0530

    Optimise build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const config: NextConfig = {
-  basePath: '/desk-calendar-app',
+  basePath: isProd ? '/desk-calendar-app' : '',
   output: 'export',
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? '/desk-calendar-app/' : '',
 };
 
 export default config;
